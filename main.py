5906c8b1df95ec0203a97ab311ba0240730b355e
LisaE14-bit <Lisa.Kwatenma@ibm.com>
2025-01-24 12:40:28 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# main.py
+def fizzbuzz(n):
+    if n % 3 == 0:
+        return "Fizz"
+    return str(n)
---
+++ test_main.py
@@ -0,0 +1,5 @@
+# test_main.py
+def test_fizzbuzz():
+    from main import fizzbuzz
+    assert fizzbuzz(3) == "Fizz"
+    
